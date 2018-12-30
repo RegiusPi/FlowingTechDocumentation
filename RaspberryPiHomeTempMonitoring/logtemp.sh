49d3a6e18de80115d78df0e43cb040330739db77
RegiusPi <loginemail.all@gmail.com>
2018-12-30 18:23:51 -0500

    moved some files

--- RaspberryPiHomeTempMonitoring/logtemp.sh
+++ RaspberryPiHomeTempMonitoring/logtemp.sh
@@ -2,5 +2,5 @@ Date="$(date --utc +'%F %T')"
 Temp=$1
 Hum=$2
 SQL="INSERT INTO GrafanaMonitoring.housetemp(date,temp,humidity) VALUES('$Date',$Temp,$Hum);"
-mysql -u wattsuser -pFlowingT3ch -h 10.0.1.32 GrafanaMonitoring -e "$SQL"
+mysql -u wattsuser -p -h 10.0.1.32 GrafanaMonitoring -e "$SQL"
 
