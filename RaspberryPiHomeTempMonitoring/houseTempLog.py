5dc496e5b9089db3a67adcf57e744169ac8a5c65
RegiusPi <loginemail.all@gmail.com>
2018-12-30 18:31:02 -0500

    commented the scripts

--- RaspberryPiHomeTempMonitoring/houseTempLog.py
+++ RaspberryPiHomeTempMonitoring/houseTempLog.py
@@ -8,6 +8,9 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_sht31d.SHT31D(i2c)
 
 while True:
+    """Loops indefinitly every five seconds. grabs the temperature through the sensor object that was 
+    instantiated above and then used the os.popen library to execute the bash script with several 
+    args"""
     hum = str(sensor.relative_humidity)
     temp = str(sensor.temperature)
     os.popen(r'./logtemp.sh ' + temp + ' ' + hum)
--- RaspberryPiHomeTempMonitoring/logtemp.sh
+++ RaspberryPiHomeTempMonitoring/logtemp.sh
@@ -1,6 +1,12 @@
+
+
+#Creates a date that is at the UTC time zone and of the format YYYY-MM-DD HH-MM-SS
 Date="$(date --utc +'%F %T')"
+#Takes the first command line argument, in this case, a temperature value that will be provided via the python script
 Temp=$1
+#Takes the second command line argument, in this case, a humidity value that will be provided via the python script
 Hum=$2
+#SQL statement to insert the Date Temp and Humidity into the housetemp table in the GrafanaMonitoring Schema
 SQL="INSERT INTO GrafanaMonitoring.housetemp(date,temp,humidity) VALUES('$Date',$Temp,$Hum);"
 mysql -u wattsuser -p -h 10.0.1.32 GrafanaMonitoring -e "$SQL"
 
